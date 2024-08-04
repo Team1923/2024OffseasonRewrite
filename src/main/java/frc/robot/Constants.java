489138b2880ec0277c81e32c36e509462fedc87d
Turtlez200 <88110937+adikeshn@users.noreply.github.com>
2024-08-04 15:25:39 -0400

    commit
    started work on arm subsystem (unfinished)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,8 +12,7 @@ import edu.wpi.first.math.util.Units;
 
 public class Constants {
 
-
-    public static class ShooterConstants{
+    public static class ShooterConstants {
 
         public static final int shooterTopID = 17;
         public static final int shooterBottomID = 18;
@@ -21,43 +20,82 @@ public class Constants {
         public static final int blowerID = 30;
 
         public static final int beamBreak4ID = 4;
-        
+
         public static final double RPSToRPM = 60;
-        public static final double RPMToRPS = 1/RPSToRPM;
+        public static final double RPMToRPS = 1 / RPSToRPM;
 
-        public static final double maxShooterAccel = 400; //Rotations/sec^2
-        public static final double maxShooterJerk = 5000; //Rotations/sec^3
+        public static final double maxShooterAccel = 400; // Rotations/sec^2
+        public static final double maxShooterJerk = 5000; // Rotations/sec^3
 
-        
-
-        //NOTE: Decided to ignore motionmagicvelocity control
+        // NOTE: Decided to ignore motionmagicvelocity control
         public static final TalonFXConfiguration CONFIGS = new TalonFXConfiguration()
-                                                            .withSlot0(new Slot0Configs() //PID
-                                                                            .withKP(0.2)
-                                                                            .withKI(0)
-                                                                            .withKD(0)
-                                                                            .withKS(0.25)
-                                                                            .withKA(0)
-                                                                            .withKV(0.115)
-                                                                        )
-                                                            .withMotionMagic(new MotionMagicConfigs()
-                                                                                .withMotionMagicAcceleration(maxShooterAccel)
-                                                                                .withMotionMagicJerk(maxShooterJerk))
-                                                            .withMotorOutput(new MotorOutputConfigs()
-                                                                            .withNeutralMode(NeutralModeValue.Brake)
-                                                                            )
-                                                            .withCurrentLimits(new CurrentLimitsConfigs() //TODO: Take a look at this (no refresh?)
-                                                                                .withStatorCurrentLimit(80)
-                                                                                .withStatorCurrentLimitEnable(true)
-                                                                            );
+                .withSlot0(new Slot0Configs() // PID
+                        .withKP(0.2)
+                        .withKI(0)
+                        .withKD(0)
+                        .withKS(0.25)
+                        .withKA(0)
+                        .withKV(0.115))
+                .withMotionMagic(new MotionMagicConfigs()
+                        .withMotionMagicAcceleration(maxShooterAccel)
+                        .withMotionMagicJerk(maxShooterJerk))
+                .withMotorOutput(new MotorOutputConfigs()
+                        .withNeutralMode(NeutralModeValue.Brake))
+                .withCurrentLimits(new CurrentLimitsConfigs() // TODO: Take a look at this (no refresh?)
+                        .withStatorCurrentLimit(80)
+                        .withStatorCurrentLimitEnable(true));
 
+        public static final double shooterRPMThreshhold = 50; // RPM
 
-        
-
-        public static final double shooterRPMThreshhold = 50; //RPM
-                                                        
     }
 
+    public static class ArmConstants {
 
+        public static final int armMotorPrimaryID = 15; // right
+        public static final int armMotorFollowerID = 16; // left
+
+        /* Motion Magic Constants */
+        public static final double armKS = 0;
+        public static final double armkP = 1.2;
+        public static final double armkI = 0.005;
+        public static final double armkD = 0;
+        public static final double maxArmVel = 100;
+        public static final double maxArmAccel = 250;
+        public static final double maxArmJerk = 1000;
+
+        /* Gearbox Ratios & Unit Conversions */
+        public static final double armGearRatio = 129.6;
+        public static final double armRotsToRads = (2 * Math.PI) / armGearRatio;
+        public static final double armRadsToRots = armGearRatio / (2 * Math.PI);
+
+        /* kG - gravity constant for motion of arm */
+        public static final double armMaxGravityConstant = 0.03 * 12; // 2 volts max ff
+
+        public static final double armPositionAllowableOffset = 0.03; // allowed radians offset
+
+        public static final double armPositionChange = 0.01; // TODO: Tune at Lehigh!!!!
+
+        public static final double armSettleTime = 0.5;
+
+        // NOTE: Decided to ignore motionmagicvelocity control
+        public static final TalonFXConfiguration CONFIGS = new TalonFXConfiguration()
+                .withSlot0(new Slot0Configs() // PID
+                        .withKP(armkP)
+                        .withKI(armkI)
+                        .withKD(armkD)
+                        .withKS(armKS)
+                        .withKA(0) // NOT SURE WHAT TO PUT HERE
+                        .withKV(0.115) // NOT SURE WHAT TO PUT HERE
+                )
+                .withMotionMagic(new MotionMagicConfigs()
+                        .withMotionMagicAcceleration(maxArmAccel)
+                        .withMotionMagicJerk(maxArmJerk))
+                .withMotorOutput(new MotorOutputConfigs()
+                        .withNeutralMode(NeutralModeValue.Brake))
+                .withCurrentLimits(new CurrentLimitsConfigs() // TODO: Take a look at this (no refresh?)
+                        .withStatorCurrentLimit(80)
+                        .withStatorCurrentLimitEnable(true));
+
+    }
 
 }
---
+++ src/main/java/frc/robot/StateCommands/ArmStateMachine.java
@@ -0,0 +1,79 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.StateCommands;
+
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.StateHandler;
+import frc.robot.Subsystems.ShooterSubsystem;
+import frc.robot.Subsystems.ShooterSubsystem.States;
+
+public class ArmStateMachine extends Command {
+
+    private ArmSubsystem armSubsystem;
+
+    private StateHandler stateHandler = StateHandler.getInstance();
+
+    /** Creates a new ShooterStateMachine. */
+    public ArmStateMachine(ArmSubsystem armSubsystem) {
+        // Use addRequirements() here to declare subsystem dependencies.
+        this.shooterSubsystem = shooterSubsystem;
+
+        addRequirements(shooterSubsystem);
+
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+
+        ShooterSubsystem.States desiredState = stateHandler.desiredShooterState;
+
+        switch (desiredState) {
+
+            case PUNT_SHOT_HIGH:
+                if (puntTimer.get() == 0) {
+                    puntTimer.start();
+                } else if (puntTimer.hasElapsed(0.5)) {
+                    stateHandler.currentShooterStates = ShooterSubsystem.States.PUNT_SHOT_HIGH;
+                }
+                break;
+
+            case RANGED:
+                // ((MotionMagicVelocityVoltage)(States.RANGED.OUTPUT)).Velocity = updated
+                // value;
+
+            default:
+                puntTimer.stop();
+                puntTimer.reset();
+                if (shooterSubsystem.isAtState(desiredState)) {
+                    stateHandler.currentShooterStates = desiredState;
+                }
+                break;
+        }
+
+        shooterSubsystem.setShooterMotorsTo(desiredState.OUTPUT_TOP, desiredState.OUTPUT_BOTTOM);
+        shooterSubsystem.setBlowerTo(stateHandler.blowerPercent);
+
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}

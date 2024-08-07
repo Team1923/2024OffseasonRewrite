fceb8db5dc4f933a719b87aeea53aa2eaf5dd36d
Griffmos <97399585+Griffmos@users.noreply.github.com>
2024-08-07 13:22:49 -0400

    GM - Arm settle timer fix
    - Found a bug with the OLD WAY of doing this
      - IF currentposition instantly became desiredposition, then it would break

--- src/main/java/frc/robot/statecommands/ArmStateMachine.java
+++ src/main/java/frc/robot/statecommands/ArmStateMachine.java
@@ -8,6 +8,7 @@ import com.ctre.phoenix6.controls.MotionMagicVoltage;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.StateHandler;
 import frc.robot.subsystems.ArmSubsystem;
@@ -17,6 +18,7 @@ public class ArmStateMachine extends Command {
 
   private ArmSubsystem armSubsystem;
   private Timer timer;
+  private ArmStates lastArmState;
 
   private StateHandler stateHandler = StateHandler.getInstance();
 
@@ -27,12 +29,16 @@ public class ArmStateMachine extends Command {
     this.armSubsystem = armSubsystem;
     timer = new Timer();
     addRequirements(armSubsystem);
+    lastArmState = ArmStates.STOWED;
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    lastArmState = stateHandler.currentArmState;
+    timer.stop();
+    timer.reset();
   }
 
   @Override
@@ -43,19 +49,30 @@ public class ArmStateMachine extends Command {
     ArmStates desiredArmState = stateHandler.desiredArmState;
 
     switch(desiredArmState){
-      case RANGED:
+      case RANGED: //Update the ranged shot's motion magic value
         // ((MotionMagicVoltage)(ArmStates.RANGED.REQUEST)).Position = updated value;
     }
 
 
-
-    if (!armSubsystem.isAtState(desiredArmState)) {
+    if (desiredArmState != stateHandler.currentArmState && armSubsystem.isAtState(desiredArmState) && timer.get() == 0){
       timer.restart();
     }
 
+
+    // if (!armSubsystem.isAtState(desiredArmState)) { //OLD WAY
+    //   System.out.println("HERE");
+    //   timer.restart();
+    // }
+
+    SmartDashboard.putNumber("TIMER", timer.get());
+
     if (timer.hasElapsed(desiredArmState.settleTime) && armSubsystem.isAtState(desiredArmState)) {
       stateHandler.currentArmState = desiredArmState;
+      timer.stop();
+      timer.reset();
     }
+
+
 
   }
 

0972318f8e2613b47bbc2e9dd3d51a41178b74d7
Griffmos <97399585+Griffmos@users.noreply.github.com>
2024-08-19 23:07:54 -0400

    GM - Made the PID tuning widget a bit smarter
    - added slot selection

--- src/main/java/frc/robot/lib/tuningwidgets/MotorPIDFVAJWidget.java
+++ src/main/java/frc/robot/lib/tuningwidgets/MotorPIDFVAJWidget.java
@@ -4,19 +4,19 @@
 
 package frc.robot.lib.tuningwidgets;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.SlotConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.shuffleboard.SuppliedValueWidget;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.ArmSubsystem.ArmStates;
 import frc.robot.subsystems.ShooterSubsystem.ShooterStates;
 
@@ -35,16 +35,25 @@ public class MotorPIDFVAJWidget {
     private GenericEntry IValue;
     private GenericEntry DValue;
     private GenericEntry FFValue;
-
+    private GenericEntry GValue;
     private GenericEntry VValue;
     private GenericEntry AValue;
-    private GenericEntry JValue;
+
+
+
+
+    private GenericEntry VeloValue;
+    private GenericEntry AccelValue;
+    private GenericEntry JerkValue;
 
     private SuppliedValueWidget<Double> currentAngle;
     private SuppliedValueWidget<Double> currentVelocity;
 
     private GenericEntry desiredAngle;
     private GenericEntry desiredVelocity;
+
+
+    private SendableChooser<Integer> slotChooser;
 
 
 
@@ -57,7 +66,7 @@ public class MotorPIDFVAJWidget {
      * @param motors
      * @param defaultConfig ASSUMES ALL MOTORS HAVE SAME DEFAULT CONFIG
      */
-    public MotorPIDFVAJWidget(String name, TalonFXConfiguration defaultConfig, int slot, double angleConversion, double velocityConversion, TalonFX... motors){
+    public MotorPIDFVAJWidget(String name, TalonFXConfiguration defaultConfig, int defaultSlot, double angleConversion, double velocityConversion, TalonFX... motors){
         tab = Shuffleboard.getTab(name);
 
         this.motors = motors;
@@ -66,42 +75,112 @@ public class MotorPIDFVAJWidget {
 
         this.angleConversion = angleConversion;
         this.velocityConversion = velocityConversion;
-
-
-        PValue = tab.add("P", defaultConfig.Slot0.kP).withPosition(0, 0).getEntry();
-        IValue = tab.add("I", defaultConfig.Slot0.kI).withPosition(1, 0).getEntry();
-        DValue = tab.add("D", defaultConfig.Slot0.kD).withPosition(2,0).getEntry();
-        FFValue= tab.add("FF (kS)", defaultConfig.Slot0.kS).withPosition(3, 0).getEntry();
         
-        VValue = tab.add("Cruise Velocity", defaultConfig.MotionMagic.MotionMagicCruiseVelocity).withPosition(0, 1).getEntry();
-        AValue = tab.add("Acceleration", defaultConfig.MotionMagic.MotionMagicAcceleration).withPosition(1, 1).getEntry();
-        JValue = tab.add("Jerk", defaultConfig.MotionMagic.MotionMagicJerk).withPosition(2, 1).getEntry();
+
+        slotChooser = new SendableChooser<>();
+
+        slotChooser.addOption("Slot0", 0);
+        slotChooser.addOption("Slot1", 1);
+        slotChooser.addOption("Slot2", 2);
+
+        SlotConfigs slot;
+
+        switch(defaultSlot){
+            case 0:
+                slot = SlotConfigs.from(defaultConfig.Slot0);
+                slotChooser.setDefaultOption("Slot0", 0);
+                break;
+            case 1:
+                slot = SlotConfigs.from(defaultConfig.Slot1);
+                slotChooser.setDefaultOption("Slot1", 1);
+                break;
+            case 2:
+                slot = SlotConfigs.from(defaultConfig.Slot2);
+                slotChooser.setDefaultOption("Slot2", 2);
+                break;
+            default:
+                slot = SlotConfigs.from(defaultConfig.Slot0);
+                System.out.println("invalid slot for " +name+" widget");
+        }
+
+
+        PValue = tab.add("P", slot.kP).withPosition(0, 0).getEntry();
+        IValue = tab.add("I", slot.kI).withPosition(1, 0).getEntry();
+        DValue = tab.add("D", slot.kD).withPosition(2,0).getEntry();
+        FFValue= tab.add("Static FF (kS)", slot.kS).withPosition(3, 0).getEntry();
+        GValue= tab.add("Gravity (kG)", slot.kG).withPosition(4, 0).getEntry();
+        VValue= tab.add("Velocity FF (kV)",slot.kV).withPosition(5, 0).getEntry();
+        AValue= tab.add("Accel FF (kA)", slot.kA).withPosition(6, 0).getEntry();
+        
+        
+        VeloValue = tab.add("Cruise Velocity", defaultConfig.MotionMagic.MotionMagicCruiseVelocity).withPosition(0, 1).getEntry();
+        AccelValue = tab.add("Acceleration", defaultConfig.MotionMagic.MotionMagicAcceleration).withPosition(1, 1).getEntry();
+        JerkValue = tab.add("Jerk", defaultConfig.MotionMagic.MotionMagicJerk).withPosition(2, 1).getEntry();
 
         if (angleConversion != 0){
-            currentAngle = tab.addDouble("CURRENT ANGLE", () -> motors[0].getPosition().getValueAsDouble() * angleConversion).withPosition(6,0).withSize(2, 1);
-            desiredAngle = tab.add("DESIRED TUNING ANGLE", ((MotionMagicVoltage)ArmStates.ANGLE_TUNING.REQUEST).Position * angleConversion).withPosition(6, 1).withSize(2, 1).getEntry();
+            currentAngle = tab.addDouble("CURRENT ANGLE", () -> motors[0].getPosition().getValueAsDouble() * angleConversion).withPosition(0,2).withSize(2, 1);
+            desiredAngle = tab.add("DESIRED TUNING ANGLE", ((MotionMagicVoltage)ArmStates.ANGLE_TUNING.REQUEST).Position * angleConversion).withPosition(0, 3).withSize(2, 1).getEntry();
 
         }
 
         if (velocityConversion != 0){
-            currentVelocity= tab.addDouble("CURRENT VELOCITY", () -> motors[0].getVelocity().getValueAsDouble() * velocityConversion).withPosition(6,0).withSize(2, 1);
-            desiredVelocity = tab.add("DESIRED TUNING VELOCITY", ((MotionMagicVelocityVoltage)ShooterStates.RPM_TUNING.REQUEST_TOP).Velocity * velocityConversion).withPosition(6, 1).withSize(2, 1).getEntry();
+            currentVelocity= tab.addDouble("CURRENT VELOCITY", () -> motors[0].getVelocity().getValueAsDouble() * velocityConversion).withPosition(2,2).withSize(2, 1);
+            desiredVelocity = tab.add("DESIRED TUNING VELOCITY", ((MotionMagicVelocityVoltage)ShooterStates.RPM_TUNING.REQUEST_TOP).Velocity * velocityConversion).withPosition(2, 3).withSize(2, 1).getEntry();
       
         }
 
 
-        tab.add("UPDATE", new InstantCommand(() -> updateMotor()).ignoringDisable(true)).withPosition(4, 1).withSize(2, 1);
+        
+
+        tab.add("SLOT", slotChooser).withPosition(4, 3);
+        tab.add("UPDATE", new InstantCommand(() -> updateMotor()).ignoringDisable(true)).withPosition(4, 2).withSize(2, 1);
     }
 
     public void updateMotor(){
-        config.Slot0.kP = PValue.get().getDouble();
-        config.Slot0.kI = IValue.get().getDouble();
-        config.Slot0.kD = DValue.get().getDouble();
-        config.Slot0.kS = FFValue.get().getDouble();
 
-        config.MotionMagic.MotionMagicCruiseVelocity = VValue.get().getDouble();
-        config.MotionMagic.MotionMagicAcceleration = AValue.get().getDouble();
-        config.MotionMagic.MotionMagicJerk = JValue.get().getDouble();
+        
+        if (slotChooser.getSelected() == -1){
+            System.out.println("NO THINGY SELECTED");
+
+        }
+        else{
+            switch(slotChooser.getSelected()){
+            case 0:
+                config.Slot0.kP = PValue.get().getDouble();
+                config.Slot0.kI = IValue.get().getDouble();
+                config.Slot0.kD = DValue.get().getDouble();
+                config.Slot0.kS = FFValue.get().getDouble();
+                config.Slot0.kG = GValue.get().getDouble();
+                config.Slot0.kV = VValue.get().getDouble();
+                config.Slot0.kA = AValue.get().getDouble();
+                break;
+            case 1:
+                config.Slot1.kP = PValue.get().getDouble();
+                config.Slot1.kI = IValue.get().getDouble();
+                config.Slot1.kD = DValue.get().getDouble();
+                config.Slot1.kS = FFValue.get().getDouble();
+                config.Slot1.kG = GValue.get().getDouble();
+                config.Slot1.kV = VValue.get().getDouble();
+                config.Slot1.kA = AValue.get().getDouble();
+                break;
+            case 2:
+                config.Slot2.kP = PValue.get().getDouble();
+                config.Slot2.kI = IValue.get().getDouble();
+                config.Slot2.kD = DValue.get().getDouble();
+                config.Slot2.kS = FFValue.get().getDouble();
+                config.Slot2.kG = GValue.get().getDouble();
+                config.Slot2.kV = VValue.get().getDouble();
+                config.Slot2.kA = AValue.get().getDouble();
+                break;
+            default:
+                System.out.println("WENT TO DEFAULT");
+        }
+        }
+        
+
+        config.MotionMagic.MotionMagicCruiseVelocity = VeloValue.get().getDouble();
+        config.MotionMagic.MotionMagicAcceleration = AccelValue.get().getDouble();
+        config.MotionMagic.MotionMagicJerk = JerkValue.get().getDouble();
 
         if (desiredAngle != null) {
             System.out.println(desiredAngle.get().getDouble() * 1/angleConversion);
